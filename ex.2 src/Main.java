99cb5bbfd8b54fe73628c940d9e8d62316ece733
Bruno <bm.machado@fct.unl.pt>
2021-10-26 18:50:42 +0100

    Adicção do State Pattern

--- ex.2 src/Main.java
+++ ex.2 src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         SmokeAlarm smokeAlarm = new SmokeAlarmClass();
+        WaterDispenser wd= new WaterDispenserStop(smokeAlarm);
         Display temperatureDisplay = new DisplayClass(smokeAlarm);
-        WaterDispenserClass wd= new WaterDispenserClass(smokeAlarm);
 
-        smokeAlarm.setChanges(7,50);
+        System.out.println("** Water Dispenser State **");
+        System.out.println(wd.getStateWaterDispenser().toString());
+        System.out.println("***************************");
+        System.out.println();
+
+        smokeAlarm.setChanges(7,23);
+
+        System.out.println();
+        System.out.println("** Water Dispenser State **");
+        System.out.println(wd.getStateWaterDispenser().toString());
+        System.out.println("***************************");
 
     }
 }
--- ex.2 src/WaterDispenser.java
+++ ex.2 src/WaterDispenser.java
@@ -1,5 +1,37 @@
-public interface WaterDispenser {
-    void turnwaterOn();
+public abstract class WaterDispenser implements Observer, WaterDispenserState {
 
-    void turnWaterOff();
+    private Subject smokeAlarm;
+    private WaterDispenser state;
+    private int smokeIntensity;
+
+
+    public WaterDispenser(Subject smokeAlarm){
+        this.smokeAlarm=smokeAlarm;
+    }
+
+
+    @Override
+    public void update(int smokeIntensity,float temperature) {
+        this.smokeIntensity=smokeIntensity;
+        if(smokeIntensity>=7) {
+            WaterDispenserStart startToFall = new WaterDispenserStart(smokeAlarm);
+            startToFall.turnWater(state);
+        }
+
+        else if(state instanceof WaterDispenserStart && smokeIntensity<7){
+            WaterDispenserStop stopToFall = new WaterDispenserStop(smokeAlarm);
+            stopToFall.turnWater(state);
+        }
+        setStateWaterDispenser(getStateWaterDispenser());
+    }
+
+    public void setStateWaterDispenser(WaterDispenser state) {
+        this.state = state;
+    }
+
+    public WaterDispenser getStateWaterDispenser() {
+        return state;
+    }
+
+    public abstract void turnWater(WaterDispenser state);
 }
